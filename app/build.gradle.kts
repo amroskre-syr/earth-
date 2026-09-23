e0bd9837b9f9350f53c379fad75336ae67ae2412
amroskre-syr <amro.skre@gmail.com>
2026-09-23 17:19:39 +0300

    Add app Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.nerva.earthwallpaper"
+    compileSdk = 35
+    defaultConfig { applicationId = "com.nerva.earthwallpaper"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
