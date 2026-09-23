e8d3e3daf913399c8989a6590e6daf4da1d88a19
amroskre-syr <amro.skre@gmail.com>
2026-09-24 01:29:22 +0300

    Make wallpaper a standalone app package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.nerva.earthwallpaper"
+        applicationId = "com.nerva.earthwallpaper.full"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0-video-match"
+        versionCode = 1
+        versionName = "1.0-standalone-video-match"
     }
 
     compileOptions {
